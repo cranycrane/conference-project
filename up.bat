e8fc98db0b8e0879fb6b1788bdc1093a19564cb4
Jakub Jerabek <jakub.jerabek@growjob.com>
2024-09-25 13:08:02 +0200

    crlf->lf

--- up.bat
+++ up.bat
@@ -1,5 +1,5 @@
 mkdir var
 mkdir log
 mkdir var\log
-docker-compose -p web-skeleton up -d --build --force-recreate
-docker exec -it web-skeleton-php /bin/bash
+docker-compose -p web-conference up -d --build --force-recreate
+docker exec -it web-conference-php /bin/bash
