8014f2b7dbf1fb1f7829d676ae5868676509d988
Jakub Jerabek <jakub.jerabek@growjob.com>
2024-10-09 15:03:30 +0200

    feat: conf detail

--- app/Domain/Conference/ConferenceRepository.php
+++ app/Domain/Conference/ConferenceRepository.php
@@ -6,7 +6,22 @@ use Doctrine\ORM\EntityRepository;
 
 class ConferenceRepository extends EntityRepository
 {
-    // Zde můžete přidat vlastní metody specifické pro konference
+
+	/**
+	 *
+	 * @return Conference[]
+	 */
+	public function find5UpcomingConferences(): array
+	{
+		return $this->createQueryBuilder('c')
+			->where('c.endsAt >= :now')
+			->setParameter('now', new \DateTime())
+			->orderBy('c.endsAt', 'ASC')
+			->setMaxResults(5)
+			->getQuery()
+			->getResult();
+	}
+
     public function findActiveConferences()
     {
         return $this->createQueryBuilder('c')
---
+++ app/UI/Modules/Front/Conference/ConferencePresenter.php
@@ -0,0 +1,86 @@
+<?php declare(strict_types = 1);
+
+namespace App\UI\Modules\Front\Conference;
+
+use App\Domain\Conference\Conference;
+use App\Domain\Presentation\Presentation;
+use App\Model\Services\ConferenceService;
+use App\Model\Services\PresentationService;
+use App\UI\Components\Conference\ConferenceForm;
+use App\UI\Components\Conference\ConferenceFormFactory;
+use App\UI\Components\Conference\ConferenceList;
+use App\UI\Components\Conference\ConferenceListFactory;
+use App\UI\Components\Presentation\PresentationForm;
+use App\UI\Components\Presentation\PresentationFormFactory;
+use App\UI\Components\Presentation\PresentationList;
+use App\UI\Components\Presentation\PresentationListFactory;
+use App\UI\Components\Room\RoomGrid;
+use App\UI\Components\Room\RoomGridFactory;
+use App\UI\Modules\Front\BaseFrontPresenter;
+use Nette\DI\Attributes\Inject;
+
+final class ConferencePresenter extends BaseFrontPresenter
+{
+	#[Inject]
+	public PresentationListFactory $presentationListFactory;
+
+	#[Inject]
+	public ConferenceListFactory $conferenceListFactory;
+
+	#[Inject]
+	public PresentationService $presentationService;
+
+	#[Inject]
+	public ConferenceService $conferenceService;
+
+	#[Inject]
+	public ConferenceFormFactory $conferenceFormFactory;
+
+	#[Inject]
+	public RoomGridFactory $roomGridFactory;
+
+	private int $conferenceId;
+
+	public function createComponentPresentationsList(): PresentationList {
+		return $this->presentationListFactory->create($this->presentationService->findByConference($this->conferenceId));
+	}
+
+	public function createComponentConferenceList(): ConferenceList {
+		return $this->conferenceListFactory->create($this->conferenceService->findAll());
+	}
+
+	public function createComponentRoomGrid(): RoomGrid {
+		return $this->roomGridFactory->create();
+	}
+
+	protected function createComponentConferenceEditForm(): ConferenceForm {
+		$form = $this->conferenceFormFactory->create();
+
+		$conference = $this->conferenceService->find($this->conferenceId);
+
+		$formComp = $form['form'];
+
+		$formComp->setDefaults([
+			'id' => $conference->getId(),
+			'title' => $conference->title,
+			'numOfPeople' => $conference->getNumOfPeople(),
+			'genre' => $conference->genre,
+			'place' => $conference->place,
+			'startsAt' => $conference->getStartsAt()->format('Y-m-d H:i:s'),
+			'endsAt' => $conference->getEndsAt()->format('Y-m-d H:i:s'),
+			'priceForSeat' => $conference->priceForSeat,
+			'capacity' => $conference->capacity,
+			'description' => $conference->description,
+		]);
+
+		return $form;
+	}
+
+	public function actionDetail(string $id): void {
+		$this->conferenceId = (int)$id;
+	}
+
+	public function renderDetail(string $id): void {
+		$this->template->conference = $this->conferenceService->find((int)$id);
+	}
+}
