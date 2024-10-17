1196f8cf800cfb9bf79776271e327b0c66da9620
Jakub Jerabek <jakub.jerabek@growjob.com>
2024-10-17 12:45:07 +0200

    feat: add/remove attendance

--- app/UI/Components/Presentation/ScheduleList.php
+++ app/UI/Components/Presentation/ScheduleList.php
@@ -2,19 +2,45 @@
 
 namespace App\UI\Components\Presentation;
 
+use App\Model\Services\AttendanceService;
 use App\Model\Services\PresentationService;
 use Doctrine\Common\Collections\ArrayCollection;
 use Nette\Application\UI\Control;
 class ScheduleList extends Control {
 
 	private PresentationService $presentationService;
+	private AttendanceService $attendanceService;
 	private ArrayCollection $presentations;
 
-	public function __construct(PresentationService $presentationService, ArrayCollection $presentations) {
+	public function __construct(PresentationService $presentationService, AttendanceService $attendanceService, ArrayCollection $presentations) {
 		$this->presentationService = $presentationService;
+		$this->attendanceService = $attendanceService;
 
 		$groupedPresentations = $this->presentationService->groupPresentationsByDay($presentations);
 		$this->presentations = $this->presentationService->sortPresentationsByTime($groupedPresentations);
+	}
+
+	public function handleAddSchedule(int $id): void {
+		try {
+			$this->attendanceService->create([
+				'presentationId' => $id,
+				'userId' => $this->presenter->getUser()->getId()
+			]);
+			$this->presenter->flashMessage('Prezentace úspěšne přidána do rozvrhu', 'success');
+		} catch (\Throwable $exception) {
+			$this->presenter->flashMessage('Nastala chyba při přidávání do rozvrhu', 'error');
+		}
+		$this->redirect('this');
+	}
+
+	public function handleRemoveSchedule(int $id): void {
+		try {
+			$this->attendanceService->delete($id);
+			$this->presenter->flashMessage('Prezentace úspěšne odebrána z rozvrhu', 'success');
+		} catch (\Throwable $exception) {
+			$this->presenter->flashMessage('Nastala chyba při odebíraní z rozvrhu ' . $exception->getMessage(), 'error');
+		}
+		$this->redirect('this');
 	}
 
 	public function render(): void {
