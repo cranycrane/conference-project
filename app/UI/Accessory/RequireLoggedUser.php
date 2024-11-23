bab9295b1b92fe8996160fe26246a56ec2e53f13
Jakub Jerabek <jakub.jerabek@growjob.com>
2024-11-23 10:14:45 +0100

    remove unused methods-files

--- app/UI/Accessory/RequireLoggedUser.php
+++ app/UI/Accessory/RequireLoggedUser.php
@@ -9,15 +9,10 @@ namespace App\UI\Accessory;
  */
 trait RequireLoggedUser
 {
-  /**
-   * Injects the requirement for a logged-in user.
-   * Attaches a callback to the startup event of the presenter.
-   */
   public function injectRequireLoggedUser(): void
   {
     $this->onStartup[] = function () {
       $user = $this->getUser();
-      // If the user isn't logged in, redirect them to the sign-in page
       if ($user->isLoggedIn()) {
         return;
       } elseif ($user->getLogoutReason() === $user::LogoutInactivity) {
